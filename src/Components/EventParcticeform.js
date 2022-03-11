b008d0bd893438be513cfff7d1e71609b8c98c40
jyb1798 <bestdung8509@gmail.com>
2022-03-11 16:51:25 +0900

    Add : setForm 연습

--- src/Components/EventParcticeform.js
+++ src/Components/EventParcticeform.js
@@ -1,12 +1,40 @@
 import React, { useState } from "react";
 
 const EventParcticeForm = () => {
-  const onClick = () => {};
+  const [form, setForm] = useState({
+    username: "",
+    message: "",
+  });
+
+  const { username, message } = form;
+
+  const onChange = (e) => {
+    const nextForm = {
+      ...form,
+      [e.target.value]: e.target.value,
+    };
+    setForm(nextForm);
+  };
+
+  const onClick = () => {
+    alert(username + ":" + message);
+  };
+
   return (
     <div>
       <h1>이벤트연습</h1>
-      <input type="text" name="username" placeholder="사용자명" />
-      <input type="text" name="message" placeholder="메세지" />
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="메세지"
+        onChange={onChange}
+      />
       <button onClick={onClick}>확인</button>
     </div>
   );
