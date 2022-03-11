45a3dfb9fff8c2bfa78d6d2788367f2fa37e9ca4
jyb1798 <bestdung8509@gmail.com>
2022-03-11 19:46:47 +0900

    Add : input state 값 여러개 한 번에 관리 추가

--- src/Components/EventParcticeform.js
+++ src/Components/EventParcticeform.js
@@ -14,10 +14,15 @@ const EventParcticeForm = () => {
       [e.target.value]: e.target.value,
     };
     setForm(nextForm);
+    console.log("하이");
   };
 
   const onClick = () => {
     alert(username + ":" + message);
+    setForm({
+      username: "",
+      message: "",
+    });
   };
 
   return (
@@ -27,12 +32,14 @@ const EventParcticeForm = () => {
         type="text"
         name="username"
         placeholder="사용자명"
+        value={username}
         onChange={onChange}
       />
       <input
         type="text"
         name="message"
         placeholder="메세지"
+        value={message}
         onChange={onChange}
       />
       <button onClick={onClick}>확인</button>
