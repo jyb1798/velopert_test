36f1dad24f0d7dd7bbb00dfea47fc675cf2d7e08
jyb1798 <bestdung8509@gmail.com>
2022-03-12 01:29:57 +0900

    Add: 컴포넌트 반복

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Test from "./Components/Test";
 import EventParctice from "./Components/EventParctice";
 import EventParcticeForm from "./Components/EventParcticeform";
+import IterationSample from "./Components/IterationSample";
 function App() {
   return (
     <>
       {/* <Test /> */}
       {/* <EventParctice /> */}
-      <EventParcticeForm />
+      {/* <EventParcticeForm /> */}
+      <IterationSample />
     </>
   );
 }
