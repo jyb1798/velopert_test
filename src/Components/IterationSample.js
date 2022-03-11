ce5aa244cd5de9ed5a238390806fd5728d9661af
jyb1798 <bestdung8509@gmail.com>
2022-03-12 08:52:45 +0900

    Add: 데이터 추가

--- src/Components/IterationSample.js
+++ src/Components/IterationSample.js
@@ -1,9 +1,31 @@
-import React from "react";
+import { useState } from "react";
 
 const IterationSample = () => {
-  const names = ["눈사람", "얼음", "눈", "바람"];
-  const nameList = names.map((name) => <li>{name}</li>);
-  return <ul>{nameList}</ul>;
+  const [names, setNames] = useState([
+    { id: 1, text: "눈사람" },
+    { id: 2, text: "얼음" },
+    { id: 3, text: "눈" },
+    { id: 4, text: "바람" },
+  ]);
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(5);
+
+  const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
+  const onChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const onClick = () => {
+    setInputText("");
+  };
+
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}> 추가 </button>
+      <ul>{nameList}</ul>;
+    </>
+  );
 };
 
 export default IterationSample;
