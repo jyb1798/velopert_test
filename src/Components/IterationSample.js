b7d53b6209555822ebc48ecb204520fb0f75c18f
jyb1798 <bestdung8509@gmail.com>
2022-03-12 09:44:10 +0900

    Add : 데이터 불변성 concat 추가

--- src/Components/IterationSample.js
+++ src/Components/IterationSample.js
@@ -2,28 +2,34 @@ import { useState } from "react";
 
 const IterationSample = () => {
   const [names, setNames] = useState([
-    { id: 1, text: "눈사람" },
-    { id: 2, text: "얼음" },
-    { id: 3, text: "눈" },
-    { id: 4, text: "바람" },
+    { id: 1, title: "눈사람" },
+    { id: 2, title: "눈" },
+    { id: 3, title: "바람" },
   ]);
-  const [inputText, setInputText] = useState("");
-  const [nextId, setNextId] = useState(5);
 
-  const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(4);
+
   const onChange = (e) => {
     setInputText(e.target.value);
   };
 
   const onClick = () => {
+    const nextName = names.concat({
+      id: nextId,
+      title: inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextName);
     setInputText("");
   };
 
+  const nameList = names.map((name) => <li key={name.id}>{name.title}</li>);
   return (
     <>
       <input value={inputText} onChange={onChange} />
-      <button onClick={onClick}> 추가 </button>
-      <ul>{nameList}</ul>;
+      <button onClick={onClick}>클릭</button>
+      <ul>{nameList}</ul>
     </>
   );
 };
